32765f42cd3a218de7c1587a2f0b485bbff6af40
Anothai Vichapaiboon <110210626+TaiChi112@users.noreply.github.com>
2025-01-29 14:27:50 +0700

    Revert "style : unlock comment coding css style"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 `"use client"`;
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import "../style/markdown.css";
+// import "./globals.css";
+// import "../style/markdown.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
