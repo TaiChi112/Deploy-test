eb83c6b96598243d408fcaed9d2ad5b9a966f657
TaiChi <anothai.0978452316@gmail.com>
2025-02-15 01:38:57 +0700

    feat: new OAuth discord

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord"; 
+
 
 export const authOptions = {
   providers: [
@@ -11,6 +13,10 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID || "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
